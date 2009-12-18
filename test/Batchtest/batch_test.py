2159d62c1ee6952467f3e3080467d1345d997c9a
Oleg Smirnov <osmirnov@gmail.com>
2009-12-18 11:08:54 +0000

    Moving Batchtest into Cattery

---
+++ test/Batchtest/batch_test.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+def run (*commands):
+  cmd = " ".join(commands);
+  print "========== $",cmd;
+  code = os.system(cmd);
+  if code:
+    raise RuntimeError,"failed with exit code %x"%code;
+
+def verify_image (file1,file2,maxdelta=1e-6):
+  import pyfits
+  im1 = pyfits.open(file1);
+  im2 = pyfits.open(file2);
+  delta = abs(im1[0].data - im2[0].data).max();
+  if delta > maxdelta:
+    raise RuntimeError,"%s and %s differ by %g"%(file1,file2,delta);
+  print "%s and %s differ by %g, this is within tolerance"%(file1,file2,delta);
+
+if __name__ == '__main__':
+
+  ## make simulated MS
+  print "========== Removing files";
+  run("rm -fr WSRT.MS* WSRT*img WSRT*fits");
+  print "========== Running makems";
+  run("makems WSRT_makems.cfg");
+  run("mv WSRT.MS_p0 WSRT.MS");
+  run("ls WSRT.MS");
+  run("lwimager ms=WSRT.MS data=CORRECTED_DATA mode=channel weight=natural npix=10");
+
+  from Timba.Apps import meqserver
+  from Timba.TDL import Compile
+  from Timba.TDL import TDLOptions
+
+  # This starts a meqserver. Note how we pass the "-mt 2" option to run two threads.
+  # A proper pipeline script may want to get the value of "-mt" from its own arguments (sys.argv).
+  print "Starting meqserver";
+  mqs = meqserver.default_mqs(wait_init=10,extra=["-mt","2"]);
+
+  try:
+    ## make simulation with perfect MODEL_DATA
+    script = "testing-sim.py";
+    print "========== Compiling",script;
+    TDLOptions.config.read("testing.tdl.conf");
+    mod,ns,msg = Compile.compile_file(mqs,script,config="simulate-model");
+    print "========== Simulating MODEL_DATA ";
+    mod._tdl_job_1_simulate_MS(mqs,None,wait=True);
+    print "========== Imaging MODEL_DATA ";
+    TDLOptions.get_job_func('Make a dirty image')(mqs,None,wait=True,run_viewer=False);
+
+    ## compare against reference image
+    print "========== Verifying test image ";
+    verify_image('WSRT.MS.MODEL_DATA.channel.1ch.fits','test-refimage.fits');
+
+    print "========== Compiling script with modified config";
+    TDLOptions.init_options("simulate-model",save=False);
+    TDLOptions.set_option("me.enable_G",True);
+    mod,ns,msg = Compile.compile_file(mqs,script,config=None);
+    print "========== Simulating DATA ";
+    TDLOptions.set_option("ms_sel.output_column","DATA");
+    mod._tdl_job_1_simulate_MS(mqs,None,wait=True);
+    print "========== Imaging DATA ";
+    TDLOptions.set_option("img_sel.imaging_column","DATA");
+    TDLOptions.get_job_func('Make a dirty image')(mqs,None,wait=True,run_viewer=False);
+
+    ## calibrate
+    script = "testing-cal.py";
+    print "========== Compiling",script;
+    mod,ns,msg = Compile.compile_file(mqs,script,config="calibrate");
+    print "========== Calibrating ";
+    TDLOptions.get_job_func('Calibrate G diagonal terms')(mqs,None,wait=True);
+    print "========== Imaging MODEL_DATA ";
+    TDLOptions.get_job_func('Make a dirty image')(mqs,None,wait=True,run_viewer=False);
+
+    ## compare against reference image
+    print "========== Verifying residual image ";
+    verify_image('WSRT.MS.CORRECTED_DATA.channel.1ch.fits','test-refresidual.fits',maxdelta=1e-3);
+
+    ## all tests succeeded
+    print "========== The hog is airborne!";
+
+  finally:
+    print "Stopping meqserver";
+    # this halts the meqserver
+    meqserver.stop_default_mqs();
+    # now we can exit
+    print "Bye!";
+
---
+++ test/Batchtest/testing-sim.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+#% $Id$
+#
+#
+# Copyright (C) 2002-2007
+# The MeqTree Foundation &
+# ASTRON (Netherlands Foundation for Research in Astronomy)
+# P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>,
+# or write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+ # standard preamble
+from Timba.TDL import *
+from Timba.Meq import meq
+import math
+
+import Meow
+import Meow.StdTrees
+
+# MS options first
+mssel = Meow.Context.mssel = Meow.MSUtils.MSSelector(has_input=False,tile_sizes=[8,16,32],flags=False);
+# MS compile-time options
+TDLCompileOptions(*mssel.compile_options());
+TDLCompileOption("run_purr","Start Purr on this MS",True);
+# MS run-time options
+TDLRuntimeOptions(*mssel.runtime_options());
+## also possible:
+# TDLRuntimeMenu("MS selection options",open=True,*mssel.runtime_options());
+
+# UVW
+TDLCompileOptions(*Meow.IfrArray.compile_options());
+
+# simulation mode menu
+SIM_ONLY = "sim only";
+ADD_MS   = "add to MS";
+SUB_MS   = "subtract from MS";
+simmode_opt = TDLCompileOption("sim_mode","Simulation mode",[SIM_ONLY,ADD_MS,SUB_MS]);
+simmode_opt.when_changed(lambda mode:mssel.enable_input_column(mode!=SIM_ONLY));
+
+# now load optional modules for the ME maker
+from Meow import MeqMaker
+meqmaker = MeqMaker.MeqMaker();
+
+# specify available sky models
+# these will show up in the menu automatically
+from Siamese.OMS import gridded_sky
+from Siamese.OMS import transient_sky
+from Siamese.OMS import fitsimage_sky
+import Meow.LSM
+lsm = Meow.LSM.MeowLSM(include_options=False);
+
+meqmaker.add_sky_models([gridded_sky,transient_sky,fitsimage_sky,lsm]);
+
+# now add optional Jones terms
+# these will show up in the menu automatically
+
+# Ncorr - correct for N
+from Siamese.OMS import oms_n_inverse
+meqmaker.add_sky_jones('Ncorr','n-term correction',oms_n_inverse);
+
+# Z - ionosphere
+from Siamese.OMS import oms_ionosphere
+meqmaker.add_sky_jones('Z','ionosphere',oms_ionosphere);
+
+# L - dipole projection
+from Siamese.OMS import oms_dipole_projection
+meqmaker.add_sky_jones('L','dipole projection',oms_dipole_projection);
+
+# E - beam
+from Siamese.OMS import analytic_beams
+from Siamese.OMS import wsrt_beams
+from Siamese.SBY import lofar_beams
+from Siamese.OMS import oms_pointing_errors
+meqmaker.add_sky_jones('E','beam',[analytic_beams,wsrt_beams,lofar_beams],
+                                  pointing=oms_pointing_errors);
+
+# P - Parallactic angle
+from Siamese.OMS import feed_angle
+meqmaker.add_uv_jones('P','feed angle',feed_angle);
+
+# G - gains
+from Siamese.OMS import oms_gain_models
+meqmaker.add_uv_jones('G','gains/phases',oms_gain_models);
+
+# very important -- insert meqmaker's options properly
+TDLCompileOptions(*meqmaker.compile_options());
+
+# noise option
+TDLCompileOption("noise_stddev","Add noise, Jy",[None,1e-6,1e-3],more=float);
+
+# MPI options
+# from Meow import Parallelization
+# TDLCompileOptions(*Parallelization.compile_options());
+
+def _define_forest (ns):
+  if not mssel.msname:
+    raise RuntimeError,"MS not set up in compile-time options";
+  if run_purr:
+    print mssel.msname;
+    import os.path
+    purrlog = os.path.normpath(mssel.msname)+".purrlog";
+    Timba.TDL.GUI.purr(purrlog,[mssel.msname,'.']);
+  # setup contexts properly
+  array,observation = mssel.setup_observation_context(ns);
+
+  # setup imaging options (now that we have an imaging size set up)
+  imsel = mssel.imaging_selector(npix=512,arcmin=meqmaker.estimate_image_size());
+  TDLRuntimeMenu("Imaging options",*imsel.option_list());
+
+  # get a predict tree from the MeqMaker
+  output = meqmaker.make_predict_tree(ns);
+
+  # throw in a bit of noise
+  if noise_stddev:
+    # make two complex noise terms per station (x/y)
+    noisedef = Meq.GaussNoise(stddev=noise_stddev)
+    noise_x = ns.sta_noise('x');
+    noise_y = ns.sta_noise('y');
+    for p in array.stations():
+      noise_x(p) << Meq.ToComplex(noisedef,noisedef);
+      noise_y(p) << Meq.ToComplex(noisedef,noisedef);
+    # now combine them into per-baseline noise matrices
+    for p,q in array.ifrs():
+      noise = ns.noise(p,q) << Meq.Matrix22(
+        noise_x(p)+noise_x(q),noise_x(p)+noise_y(q),
+        noise_y(p)+noise_x(q),noise_y(p)+noise_y(q)
+      );
+      ns.noisy_predict(p,q) << output(p,q) + noise;
+    output = ns.noisy_predict;
+
+  # in add or subtract sim mode, make some spigots and add/subtract visibilities
+  if sim_mode == ADD_MS:
+    spigots = array.spigots();
+    for p,q in array.ifrs():
+      ns.sum(p,q) << output(p,q) + spigots(p,q);
+    output = ns.sum;
+  elif sim_mode == SUB_MS:
+    spigots = array.spigots();
+    for p,q in array.ifrs():
+      ns.diff(p,q) << spigots(p,q) - output(p,q);
+    output = ns.diff;
+  else:
+    spigots = False;
+
+  # make sinks and vdm.
+  # The list of inspectors comes in handy here
+  Meow.StdTrees.make_sinks(ns,output,spigots=spigots,post=meqmaker.get_inspectors());
+
+  # very important -- insert meqmaker's options properly
+  TDLRuntimeOptions(*meqmaker.runtime_options());
+
+  # close the meqmaker. This produces annotations, etc.
+  meqmaker.close();
+
+
+def _tdl_job_1_simulate_MS (mqs,parent,wait=False):
+  mqs.execute('VisDataMux',mssel.create_io_request(),wait=wait);
+
+
+# this is a useful thing to have at the bottom of the script, it allows us to check the tree for consistency
+# simply by running 'python script.tdl'
+
+if __name__ == '__main__':
+  ns = NodeScope();
+  _define_forest(ns);
+  # resolves nodes
+  ns.Resolve();
+
+  print len(ns.AllNodes()),'nodes defined';
